3fc662656cac8442951afa5f0138f2fcfd1fad94
Artem Dolya <artem.dolya@gmail.com>
2024-03-07 13:17:39 -0500

    Added Postgres ServiceCollection Extensions

---
+++ src/Net/DependencyInjection/KrapkaNet.DependencyInjection.Postgres/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using KrapkaNet.EntityFramework.Abstractions;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace KrapkaNet.DependencyInjection.Postgres
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddPostgresDbContext<TContext>(this IServiceCollection services, string connectionString)
+            where TContext : ApplicationDbContext
+        {
+            services.AddDbContext<TContext>((serviceProvider, dbContextOptionsBuilder) =>
+            {
+                dbContextOptionsBuilder.UseNpgsql(
+                    serviceProvider.GetRequiredService<IConfiguration>().GetConnectionString(connectionString),
+                    npgsqlOptionsBuilder =>
+                        npgsqlOptionsBuilder.MigrationsAssembly(typeof(TContext).Assembly.FullName));
+            });
+        
+            return services;
+        }
+    }
+}
